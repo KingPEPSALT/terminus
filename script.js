03f6017321e32c95524e406fe68f2e57e04403c4
Quin <68469008+KingPEPSALT@users.noreply.github.com>
2022-07-19 15:23:55 +0100

    v0.1.6, relative and absolute paths!

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ setInterval(() => {
     cursor = !cursor;
 }, speed);
 
+
+const filename_regex = /^[a-zA-Z0-9_\-*()£$!?]+\.[a-zA-Z0-9]+$/
+const dirname_regex = /^[a-zA-Z0-9_\-*()£$!?]+$/
+
 class FileSystemObject{
     constructor(name, parent){
         this.name = name;
@@ -36,6 +40,9 @@ class FileSystemObject{
     show(){
         return "<span class=\""+(this instanceof Directory ? "dir" : "file")+"\">"+this.name+"</span>";
     }
+
+    
+
 }
 class Directory extends FileSystemObject{
     constructor(name, subdirectories = [], parent){
@@ -50,11 +57,38 @@ class Directory extends FileSystemObject{
         return obj;
     }
 
-    find(name, directory){
+    find(name, is_directory){
+        if(name == ".."){
+            if(is_directory === false) return null;
+            return this.parent;
+        };
         let idx = -1;
-        if(directory === undefined) idx = this.subdirectories.findIndex(element=>element.name===name);
-        else idx = this.subdirectories.findIndex(element=>element.name===name&&(directory ? element instanceof Directory : element instanceof FileObj));
+        if(is_directory === undefined) idx = this.subdirectories.findIndex(element=>element.name===name);
+        else idx = this.subdirectories.findIndex(element=>element.name===name&&(is_directory ? element instanceof Directory : element instanceof FileObj));
         return idx===-1 ? null : this.subdirectories[idx];
+    }
+
+    from_path(path, to_make = false){
+        path = path.trim();
+
+        let cursor = this;
+        if (path.startsWith("~")) cursor = root;
+        let arr_path = path.split("/");
+        let target = arr_path.pop();
+        for(let dir of arr_path){
+            if (dir === ".") continue;
+            if (dir === "..") {
+                if (cursor == root) return null;
+                cursor = cursor.parent; 
+                continue;
+            };
+            let next = cursor.find(dir);
+            if (cursor === null) return null;
+            if (next instanceof FileObj) return null;
+            cursor = next;
+        }
+        if (to_make) return [cursor, target];
+        else return cursor.find(target)
     }
 
     display(){
@@ -69,10 +103,19 @@ class FileObj extends FileSystemObject{
     }
 }
 
-const root = new Directory("root", [                    
+const root = new Directory("~", [                    
     new Directory("documents", [new FileObj("about.txt", "All about me!"), new FileObj("this.txt", "All about this...")]),
+    new FileObj("changelog.txt", "\
+        <strong>patch v0.1.6</strong><br>File path's now added! Try: <span class=\"cmd\">edit</span> documents/about.txt!<br><br><br>\
+        <strong>patch v0.1.5</strong><br>CRT filter added! Looks neat!<br><br><br>\
+        <strong>patch v0.1.4</strong><br>Github! Now hosted on github pages!<br><br><br>\
+        <strong>patch v0.1.3</strong><br>Big update! Removed the command <span class=\"cmd\">inp</span> for the way better command <span class=\"cmd\">edit</span><br><br><br>\
+        <strong>patch v0.1.2</strong><br>Updated the code for cleanliness and ease of use! File/Folder objects should be much easier to use!<br><br><br>\
+        <strong>patch v0.1.1</strong><br>Can now edit and create files and directories, use the <span class=\"cmd\">mk, new</span> and <span class=\"cmd\">inp</span> commands to create!<br><br><br> \
+        <strong>patch v0.1.0</strong><br>Basic filesystem created and traversable, use the <span class=\"cmd\">cat, cd</span> and <span class=\"cmd\">ls</span> commands to traverse!"
+    ),
     new Directory("pictures", [new FileObj("kitten.jpg", "Images aren't supported rn :(")])
-]);
+], null);
 let current_dir = root;
 let editing_file = false;
 let current_file = null;
@@ -82,56 +125,51 @@ let command_list = {
     "ls":(x)=>{
         if(x.length == 0) return current_dir.display();
         else{
-            let dir = current_dir.find(x.join(" "), true);
-            if (dir) return dir.display();
-            return "could not find folder '"+x.join(" ")+"'";
+            let dir = current_dir.from_path(x[0]);
+            if (dir instanceof Directory) return dir.display();
+            return "could not find folder '"+x[0]+"'";
         }
     },
     "cat":(x)=>{
-        let file = current_dir.find(x.join(" "), false);
-        if (file) return file.content; 
-        return "could not find file '"+x.join(" ")+"'";
+        let file = current_dir.from_path(x[0]);
+        if (file instanceof FileObj) return file.content; 
+        return "could not find file '"+x[0]+"'";
     },
     "cd":(x)=>{
-        if (x.join(" ") === ".." && current_dir.parent != undefined) current_dir = current_dir.parent
-        else if (x.join(" ") === "." || x.length === 0) return current_dir.path();
-        else{
-            let dir = current_dir.find(x.join(" "), true);
-            if (dir) current_dir = dir;
-            else return "could not find folder '"+x.join(" ")+"'";
-        }
-        return current_dir.path();
-        
+        let dir = current_dir.from_path(x[0]);
+        if (dir instanceof Directory) current_dir = dir;
+        else return "could not find folder '"+x[0]+"'";
+        return "";
     },
     "mk":(x)=>{
-        let dir = current_dir.find(x.join(" "), true);
-        if (dir) return "a directory," + dir.show() + ", already exists.";
-        return "directory " + current_dir.add(new Directory(x[0])).show() + " successfully created";
+        let dirname = x[0]
+        let obj = current_dir.from_path(x[0], true);
+        if (!obj) return x[0].split("/").pop().join("/")+" could not be resolved as a directory.";
+        if (obj[1] != undefined) dirname = obj[1];
+        if (!dirname_regex.test(dirname)) return "invalid directory name.";
+        if (obj[0].find(obj[1])) return "an object, " + obj[0].find(obj[1]).show() + ", already exists.";
+        return "directory " + obj[0].add(new Directory(dirname)).show() + " successfully created";
     },
     "new":(x)=>{
-        let file = current_dir.find(x.join(" "), false);
-        if (file) return "a file, " + file.show() + ", already exists.";
-        return "file " + current_dir.add(new FileObj(x[0])).show() + " successfully created."
+        let filename = x[0];
+        let obj = current_dir.from_path(x[0], true);
+        if (!obj) return x[0].split("/").pop().join("/")+" could not be resolved as a directory.";
+        if (obj[1] != undefined) filename = obj[1];
+        if (!filename_regex.test(filename)) return "invalid file name.";
+        if (obj[0].find(obj[1])) return "an object, " + obj[0].find(obj[1]).show() + ", already exists";
+        return "file " + obj[0].add(new FileObj(obj[1])).show() + " successfully created."
     },
     "edit":(x)=>{
         if(x.length == 0) return "a filename is required."
         let filename = x.shift();
-        let file = current_dir.find(filename, false);
-        if (!file) return "could not find file '"+filename+"'";
+        let file = current_dir.from_path(filename);
+        if (!(file instanceof FileObj)) return "could not find file '"+filename+"'";
         current_file = file; editing_file = true;
         document.getElementById("edit-panel").style.display = "flex";
         document.getElementById("edit-file").value = file.content;
         document.getElementById("edit-file").focus();
         return "";
-    },
-    "changelog":()=>
-        "\
-        <strong>patch v0.1.5</strong><br>CRT filter added! Looks neat!<br><br><br>\
-        <strong>patch v0.1.4</strong><br>Github! Now hosted on github pages!<br><br><br>\
-        <strong>patch v0.1.3</strong><br>Big update! Removed the command <span class=\"cmd\">inp</span> for the way better command <span class=\"cmd\">edit</span><br><br><br>\
-        <strong>patch v0.1.2</strong><br>Updated the code for cleanliness and ease of use! File/Folder objects should be much easier to use!<br><br><br>\
-        <strong>patch v0.1.1</strong><br>Can now edit and create files and directories, use the <span class=\"cmd\">mk, new</span> and <span class=\"cmd\">inp</span> commands to create!<br><br><br> \
-        <strong>patch v0.1.0</strong><br>Basic filesystem created and traversable, use the <span class=\"cmd\">cat, cd</span> and <span class=\"cmd\">ls</span> commands to traverse! "
+    }
     
 }
 function close_file(){
@@ -155,7 +193,7 @@ document.addEventListener('keydown', (e)=>{
     if (e.key == "Enter") {
         let inp = (in_before.innerHTML+in_after.innerHTML).trim().split(" ");
         let command = inp.shift();
-        document.getElementById("history").innerHTML+= "$ " + command + " " + inp.join(" ")
+        document.getElementById("history").innerHTML+= "<span class=\"dir\">"+current_dir.path() + "</span> $ " + command + " " + inp.join(" ")
         let output = "";
         if (command_list[command] == undefined && command!="help") output = "'" + command + "' is not a valid command. Type <span class=\"cmd\">help</span> for a list of commands.";
         else if (command=="help") {
@@ -168,6 +206,7 @@ document.addEventListener('keydown', (e)=>{
         if (command != "clear") document.getElementById("history").innerHTML += "<br>" + output + (output.length==0 ? "":"<br>") + "<br>";
         in_before.innerHTML="";
         in_after.innerHTML="";
+        document.getElementById("cur-dir").innerHTML = current_dir.path();
     }
     else if (e.key == "Backspace"){
         in_before.innerHTML = in_before.innerHTML.slice(0, in_before.innerHTML.length-1);
